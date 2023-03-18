9a14f9892816ce7809812574639a289d1b0983a1
Yunus Emre <128180132+YunusEmreAy@users.noreply.github.com>
2023-03-18 19:36:36 +0300

    Update Main.py

--- Amiral Battı Oyunu/Main.py
+++ Amiral Battı Oyunu/Main.py
@@ -1,4 +1,4 @@
-# Yunus Emre Ay / 21100011016 / Tel:05516524768 / E-posta:TR.yunus.emre.ay@gmail.com / <3
+# Yunus Emre Ay / E-posta:TR.yunus.emre.ay@gmail.com
 import random
 while True:
     print("*****Amiral Battı Oyununa Hosgeldiniz*****\n\n")
@@ -32,7 +32,7 @@ while True:
     #                     ["?","?","?","?","?","?","?","?","?","?"]] 9
     #                       0   1   2   3   4   5   6   7   8   9
     #                                                                           ~~YANDA GORDUGUNUZ SEKILLER BENIM MATRISLERIMIN
-    # Oyun_Alani_Acik = [["?","?","?","?","?","?","?","?","?","?"], 0             SEKILLERI.~~ (Matrislerimi 14-20 satirlari arasinda
+    # Oyun_Alani_Acik = [["?","?","?","?","?","?","?","?","?","?"], 0             SEKILLERI.~~ (Matrislerimi 14-22 satirlari arasinda
     #                    ["?","?","?","?","?","?","?","?","?","?"], 1             for döngüleriyle olusturdum, bu sekiller sadece
     #                    ["?","?","?","?","?","?","?","?","?","?"], 2             gorselligi arttırabilmek icin)
     #                    ["?","?","?","?","?","?","?","?","?","?"], 3
